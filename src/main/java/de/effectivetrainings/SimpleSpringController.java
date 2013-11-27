069cbe0c65b4600583949de6f193ecc9940c1990
dilgerm <martin@effectivetrainings.de>
2013-11-27 07:11:43 +0100

    Setting up spring boot

---
+++ src/main/java/de/effectivetrainings/SimpleSpringController.java
@@ -0,0 +1,66 @@
+/**
+ Copyright (c) 2013, Martin Dilger - Effective Trainings & Consulting - EffectiveTrainings.de
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of Effective Trainings & Consulting nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL EffectiveTrainings BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package de.effectivetrainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author <a href=mailto:martin@effectivetrainings.de">Martin Dilger</a>
+ * @since: 26.11.13
+ */
+@Controller
+@EnableAutoConfiguration
+public class SimpleSpringController {
+
+    public SimpleSpringController() {
+        System.out.println("Log");
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET,
+        value = "/hello/{helloValue}",
+        produces = MediaType.APPLICATION_XML_VALUE
+    )
+    @ResponseBody
+    public Hello sayHello(@PathVariable(value = "helloValue") String hello){
+        return new Hello(hello);
+    }
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SimpleSpringController.class, args);
+    }
+}
